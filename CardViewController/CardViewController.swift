5fd0da92195a6a72317f0d2f3210b0f68569e386
Mohamed Hashem <mohamedhashem@xtrava.co>
2020-09-29 14:21:51 +0200

    init

---
+++ CardViewController/CardViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CardViewController.swift
+//  CardViewController
+//
+//  Created by mohamed hashem on 9/29/20.
+//  Copyright © 2020 mohamed hashem. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+    
+ @IBOutlet weak var handleArea: UIView!
+
+}
