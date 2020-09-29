50f66117556ef39cd5460347a67dfb8315f37d51
Mohamed Hashem <mohamedhashem@xtrava.co>
2020-09-29 14:16:45 +0200

    Initial Commit

---
+++ CardViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardViewController
+//
+//  Created by mohamed hashem on 9/29/20.
+//  Copyright © 2020 mohamed hashem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
